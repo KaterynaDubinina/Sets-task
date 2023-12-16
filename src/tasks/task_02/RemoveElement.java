a51ae053bee9c02ca6210a4446f7d53b52a529de
KaterynaDubinina <ekaterynadubinina@gmail.com>
2023-12-16 22:38:42 +0200

    Initial commit

---
+++ src/tasks/task_02/RemoveElement.java
@@ -0,0 +1,38 @@
+package tasks.task_02;
+
+import java.util.HashSet;
+
+public class RemoveElement {
+
+    public static void main(String[] args) {
+
+        System.out.println("\n\u2714\uFE0F Initial data:");
+        getOutput(getInitialSet());
+
+        System.out.println("\n\u267B\uFE0F Updated data:");
+        getOutput(getUpdatedSet(new HashSet<>(getInitialSet())));
+    }
+
+    private static HashSet<String> getInitialSet() {
+        HashSet<String> set = new HashSet<>();
+        set.add("orange     \uD83C\uDF4A");
+        set.add("apple      \uD83C\uDF4F");
+        set.add("kiwi       \uD83E\uDD5D");
+        set.add("strawberry \uD83C\uDF53");
+        set.add("mango      \uD83E\uDD6D");
+        return set;
+    }
+
+    private static HashSet<String> getUpdatedSet(HashSet<String> set) {
+        set.removeIf(element -> element.contains("kiwi       \uD83E\uDD5D"));
+        return set;
+    }
+
+    private static void getOutput(HashSet<String> set) {
+        int i = 1;
+        for (String element : set) {
+            System.out.println(i + ") " + element);
+            i++;
+        }
+    }
+}
